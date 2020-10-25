ba20ffb41b865e0115e6aac23edf8b9af3897b92
Valentin Chassignol <valentin.chassignol@epita.fr>
2020-10-25 16:01:06 +0100

    feat: Adding softmax and mse functions

---
+++ src/neuralnetwork/function.h
@@ -0,0 +1,44 @@
+#ifndef CTEXT_SRC_NEURALNETWORK_FUNCTION_H_
+#define CTEXT_SRC_NEURALNETWORK_FUNCTION_H_
+
+#include <stdlib.h>
+
+/**
+ * Softmax activation.
+ *
+ * @date    25/10/2020
+ * @version 0.0.1
+ * @author  Vinetos
+ * @package network
+ *
+ * This method activates the given input array using the softmax
+ * classification method.
+ *
+ * @warning This method modify the given array.
+ *
+ * @throw error  when size < 0.
+ * @param inputs the arrays of values to activate.
+ * @param size   the size of the array
+ *
+ */
+void softmax(double *inputs, size_t size);
+
+/**
+ * Mean squared error.
+ *
+ * @date    25/10/2020
+ * @version 0.0.1
+ * @author  Vinetos
+ * @package network
+ *
+ * This method calculate the mean squared error between the input and the
+ * expected output.
+ *
+ * @param input      The activated value of the neuron
+ * @param expected   The expected value
+ *
+ * @return the mean squared error value.
+ */
+double mse(double input, double expected);
+
+#endif //CTEXT_SRC_NEURALNETWORK_FUNCTION_H_
