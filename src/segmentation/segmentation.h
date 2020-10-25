602575ed79719fdcb04c21fbb53c12cdaacaa823
matthieu BARONNET <matthieu.baronnet@epita.fr>
2020-10-25 19:12:50 +0100

    seg structure done, added segmentation.h, program can segment but return not yet functionnal

---
+++ src/segmentation/segmentation.h
@@ -0,0 +1,10 @@
+#ifndef SEGMENTATION_H
+#define SEGMENTATION_H
+
+void hori_histo(unsigned int ext[], size_t sizex, size_t sizey, unsigned char img[sizey][sizex]);
+void vert_histo(size_t upper,size_t lower,unsigned int ext[], size_t sizex, size_t sizey, unsigned char img[sizey][sizex], size_t left, size_t right);
+void letter_seg(size_t upper,size_t lower, size_t left, size_t right, size_t sizex, size_t sizey, unsigned char img[sizey][sizex]);
+void word_seg(size_t upper,size_t lower,size_t sizex, size_t sizey, unsigned char img[sizey][sizex]);
+void line_seg(size_t sizex, size_t sizey, unsigned char img[sizey][sizex]);
+
+#endif
