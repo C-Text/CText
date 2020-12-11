4221406182d937de0900e38922cf0dd4742bd7f4
Valentin Chassignol <Vinetosdev@gmail.com>
2020-12-11 14:12:55 +0100

    feat: User can select an image and preview it

--- src/libs/application/application.c
+++ src/libs/application/application.c
@@ -1,45 +1,53 @@
 #include "application.h"
 
-#define W_MAIN_ID "org.ctext.main_window"
-#define W_DIALG_ID "org.ctext.file_chooser"
+#define W_MAIN_ID     "org.ctext.main_window"
+#define W_DIALG_ID    "org.ctext.file_chooser"
+#define BTN_DIALG_OK  "org.ctext.file_chooser.btn_ok"
+#define IMG_PREVIEWER "org.ctext.img_previewer"
 
-G_MODULE_EXPORT void __attribute__ ((unused))on_button1_clicked(__attribute__ ((unused))GtkButton *button, __attribute__ ((unused))gpointer user_data) {
-    g_print("Check point 1\n");
-}
-
-G_MODULE_EXPORT void __attribute__ ((unused))on_button2_clicked(__attribute__ ((unused))GtkButton *button, __attribute__ ((unused))gpointer user_data) {
-    g_print("Check point 2\n"); //to help debugging
+void on_file_selected(__attribute__ ((unused))GtkButton *button,
+                      app_widgets* widgets) {
+  gtk_image_set_from_file(widgets->img_previewer,
+                          gtk_file_chooser_get_filename(widgets->w_dlg_file_choose));
+  gtk_widget_destroy (GTK_WIDGET(widgets->w_dlg_file_choose));
 }
 
 int launch_application(int argc, char **argv) {
-    GtkBuilder *builder;
-    GError *error = NULL;
-    app_widgets *widgets = g_slice_new(app_widgets);
+  GtkBuilder *builder;
+  GError *error = NULL;
+  app_widgets *widgets = g_slice_new(app_widgets);
 
-    // Init gtk
-    gtk_init(&argc, &argv);
+  // Init gtk
+  gtk_init(&argc, &argv);
 
-    builder = gtk_builder_new();
-    if (gtk_builder_add_from_file(builder, "src/assets/interface.glade", &error) == 0) {
-        g_printerr("Error loading file: %s\n", error->message);
-        g_clear_error(&error);
-        return 1;
-    }
+  builder = gtk_builder_new();
+  if (gtk_builder_add_from_file(builder, "src/assets/interface.glade", &error)
+      == 0) {
+    g_printerr("Error loading file: %s\n", error->message);
+    g_clear_error(&error);
+    return 1;
+  }
 
-    /* Connect signal handlers to the constructed widgets. */
-    widgets->w_main = GTK_WIDGET(gtk_builder_get_object(builder, W_MAIN_ID));
-    widgets->w_dlg_file_choose = GTK_WIDGET(gtk_builder_get_object(builder, W_DIALG_ID));
+  /* Connect signal handlers to the constructed widgets. */
+  widgets->w_main = GTK_WIDGET(gtk_builder_get_object(builder, W_MAIN_ID));
+  widgets->w_dlg_file_choose =
+      GTK_WIDGET(gtk_builder_get_object(builder, W_DIALG_ID));
+  widgets->btn_ok = GTK_BUTTON(gtk_builder_get_object(builder, BTN_DIALG_OK));
+  widgets->img_previewer = GTK_IMAGE(gtk_builder_get_object(builder, IMG_PREVIEWER));
 
-    // Connect signals
-    gtk_builder_connect_signals(builder, widgets);
+  // Connect signals
+  gtk_builder_connect_signals(builder, widgets);
 
-    g_object_unref(builder);
+  g_object_unref(builder);
 
-    g_signal_connect(widgets->w_main, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-    gtk_widget_show(widgets->w_main);
+  g_signal_connect(widgets->w_main, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+  g_signal_connect(widgets->btn_ok,
+                   "clicked",
+                   G_CALLBACK(on_file_selected),
+                   widgets);
+  gtk_widget_show(widgets->w_main);
 
-    gtk_main();
+  gtk_main();
 
-
-    return 0;
+  return 0;
 }
--- src/libs/application/application.h
+++ src/libs/application/application.h
@@ -4,8 +4,10 @@
 #include <gtk/gtk.h>
 
 typedef struct {
-    GtkWidget *w_main;                  // Pointer to text view object
-    GtkWidget *w_dlg_file_choose;       // Pointer to file chooser dialog box
+  GtkWidget *w_main;                      // Pointer to text view object
+  GtkFileChooser *w_dlg_file_choose;      // Pointer to file chooser dialog box
+  GtkButton *btn_ok;                      // Ok buton in file chooser
+  GtkImage *img_previewer;                // Image previewer on the main window
 } app_widgets;
 
 int launch_application(int argc, char **argv);
