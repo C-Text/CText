cc54887e8dfa5e89078de7078e0795b3416c8483
Valentin Chassignol <Vinetosdev@gmail.com>
2020-12-11 15:59:42 +0100

    feat: Resize image when resizing windows

--- src/libs/application/application.c
+++ src/libs/application/application.c
@@ -4,11 +4,43 @@
 #define W_DIALG_ID    "org.ctext.file_chooser"
 #define BTN_DIALG_OK  "org.ctext.file_chooser.btn_ok"
 #define IMG_PREVIEWER "org.ctext.img_previewer"
+#define LAYOUT_IMG    "org.ctext.layout_img"
+
+gboolean resize_image(GtkWidget *widget, GdkRectangle *allocation,
+                       app_widgets* widgets) {
+  int x,y,w,h;
+  GdkPixbuf *pxbscaled;
+  GtkWidget *image = GTK_WIDGET(widgets->img_previewer);
+  GdkPixbuf *pixbuf= widgets->pix_buf;
+
+  x = 0;
+  y = 0;
+
+  h = allocation->height;
+  w = (gdk_pixbuf_get_width(pixbuf) * h) / gdk_pixbuf_get_height(pixbuf);
+
+  pxbscaled = gdk_pixbuf_scale_simple(pixbuf, w, h, GDK_INTERP_BILINEAR);
+
+  if (w < allocation->width) {
+    x = (allocation->width - w) / 2;
+    gtk_layout_move(GTK_LAYOUT(widget), image, x, y);
+  }
+
+  gtk_image_set_from_pixbuf(GTK_IMAGE(image), pxbscaled);
+  g_object_unref (pxbscaled);
+  return FALSE;
+}
 
 void on_file_selected(__attribute__ ((unused))GtkButton *button,
                       app_widgets* widgets) {
-  gtk_image_set_from_file(widgets->img_previewer,
-                          gtk_file_chooser_get_filename(widgets->w_dlg_file_choose));
+
+  gchar *filename = gtk_file_chooser_get_filename(widgets->w_dlg_file_choose);
+  gtk_image_set_from_file(widgets->img_previewer, filename);
+  widgets->pix_buf = gdk_pixbuf_new_from_file (filename, NULL);
+  g_signal_connect(widgets->layout_img,
+                   "size-allocate",
+                   G_CALLBACK(resize_image),
+                   widgets);
   gtk_widget_destroy (GTK_WIDGET(widgets->w_dlg_file_choose));
 }
 
@@ -30,24 +62,31 @@ int launch_application(int argc, char **argv) {
 
   /* Connect signal handlers to the constructed widgets. */
   widgets->w_main = GTK_WIDGET(gtk_builder_get_object(builder, W_MAIN_ID));
+  widgets->layout_img = GTK_LAYOUT(gtk_builder_get_object(builder, LAYOUT_IMG));
   widgets->w_dlg_file_choose =
-      GTK_WIDGET(gtk_builder_get_object(builder, W_DIALG_ID));
+      GTK_FILE_CHOOSER(gtk_builder_get_object(builder, W_DIALG_ID));
   widgets->btn_ok = GTK_BUTTON(gtk_builder_get_object(builder, BTN_DIALG_OK));
   widgets->img_previewer = GTK_IMAGE(gtk_builder_get_object(builder, IMG_PREVIEWER));
 
   // Connect signals
   gtk_builder_connect_signals(builder, widgets);
 
+
   g_object_unref(builder);
 
-  g_signal_connect(widgets->w_main, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+  g_signal_connect(widgets->w_main,
+                   "destroy",
+                   G_CALLBACK(gtk_main_quit), NULL);
   g_signal_connect(widgets->btn_ok,
                    "clicked",
                    G_CALLBACK(on_file_selected),
                    widgets);
+
   gtk_widget_show(widgets->w_main);
 
   gtk_main();
+
+  g_free(widgets);
 
   return 0;
 }
--- src/libs/application/application.h
+++ src/libs/application/application.h
@@ -2,12 +2,15 @@
 #define CTEXT_APPLICATION_H_
 
 #include <gtk/gtk.h>
+#include <gdk-pixbuf/gdk-pixbuf.h>
 
 typedef struct {
   GtkWidget *w_main;                      // Pointer to text view object
   GtkFileChooser *w_dlg_file_choose;      // Pointer to file chooser dialog box
   GtkButton *btn_ok;                      // Ok buton in file chooser
   GtkImage *img_previewer;                // Image previewer on the main window
+  GdkPixbuf *pix_buf;
+  GtkLayout *layout_img;
 } app_widgets;
 
 int launch_application(int argc, char **argv);
