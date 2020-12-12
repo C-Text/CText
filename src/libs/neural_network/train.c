4791d3b3f6944bd90c0ed3c86657ad05b2a1dc9e
Valentin Chassignol <Vinetosdev@gmail.com>
2020-12-12 19:17:15 +0100

    feat: Allow to only train the network

--- src/libs/neural_network/train.c
+++ src/libs/neural_network/train.c
@@ -26,7 +26,6 @@ double *get_matrix(SDL_Surface *image) {
     for (int j = 0; j < length; j++) {
       if (i >= image->w || j >= image->h) {
         array[i * length + j] = 0;
-        printf("%d", 0);
       } else {
         Uint32 pixel = (get_pixel(image, i, j));
         SDL_GetRGB(pixel, image->format, &r, &b, &g);
@@ -36,30 +35,73 @@ double *get_matrix(SDL_Surface *image) {
           array[i * length + j] = 1;
         else
           array[i * length + j] = 0;
-        printf("%d", r < 240 ? 1 : 0);
       }
     }
-    printf("\n");
   }
-  printf("\n");
   return array;
 }
+
+size_t run(NeuralNetwork *network, double entry[]) {
+  // Propagate the info with current letter
+  propagation(network, entry);
+  double max_proba = 0;
+  Node *output_neurons = ((List) (network->layers->last->value))->first;
+  Neuron *n;
+
+  // Find th index of the max probability of the output
+  size_t i_index = 0;
+  for (unsigned long i = 0; i < ((List) (network->layers->last->value))->length; i++) {
+    n = (Neuron *) (output_neurons->value);
+    if (max_proba < n->value) {
+      max_proba = n->value;
+      i_index = i;
+    }
+    output_neurons = output_neurons->next;
+  }
+  return i_index;
+}
+
 
 void training() {
   // Save all images into an array
   double **models = malloc(sizeof(char *) * 74);
+  double *expected = malloc(sizeof (double ) * 74);
 
-  char paths[] = "src/assets/font-01/letter001.bmp";
+  char paths[] = "src/assets/font-01/letter000.bmp";
 
   int index_in = 0;
-  //for (int m = 0; m < 74; m++) {
+  for (int m = 0; m < 74; m++) {
       SDL_Surface *image = SDL_LoadBMP(paths);
       models[index_in] = get_matrix(image);
       index_in++;
       if (paths[27] == '9') {
         paths[27] = '0';
         paths[26]++;
-      }
-  //}
+      } else
+        paths[27]++;
+  }
 
+  size_t nb_neurons_per_layer[4] = {
+      1024, 100,100,74
+  };
+  NeuralNetwork network;
+
+  init(&network, 4, nb_neurons_per_layer);
+  for (int i = 0; i < 10000; i++) {
+    if (i % 100 == 0) {
+      printf("%d\n", i);
+      save_neural_network(&network, "src/assets/trained-network.txt");
+    }
+
+    for(int c = 0; c < 74; c++) {
+      propagation(&network, models[c]);
+      expected[c] = 1;
+      backpropagation(&network, expected, 0.1);
+      expected[c] = 0;
+    }
+  }
+  for(int c = 0; c < 74; c++) {
+    printf("%c -> %c\n", result_elements[c], result_elements[run(&network,
+                                                                models[c])]);
+  }
 }
--- src/main.c
+++ src/main.c
@@ -1,33 +1,16 @@
-#include <stdio.h>
-#include "libs/neural_network/neuralnetwork.h"
 #include "libs/neural_network/train.h"
 #include "libs/application/application.h"
 
 int main(int argc, char **argv) {
-  training();
-  return 0;
   if (argc == 1) {
     // Launch interface
     return launch_application(argc, argv);
   }
-
-  printf("Called: %s\n", argv[0]);
-  double inputs[1][2] = {
-      {0.05, 0.1}
-  };
-  double outputs[1][2] = {
-      {0.05, 0.80},
-  };
-  size_t nb_neurons_per_layer[3] = {
-      2, 2, 2
-  };
-  NeuralNetwork network;
-
-  init(&network, 3, nb_neurons_per_layer);
-  for (int i = 0; i < 1000; i++) {
-    propagation(&network, inputs[0]);
-    backpropagation(&network, outputs[0], 0.5);
+  // ctext -t : train the network
+  if (argc == 2 && argv[1][0] == '-' && argv[1][1] == 't') {
+    training();
+    return 0;
   }
-  propagation(&network, inputs[0]);
+
   return 0;
 }
